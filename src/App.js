57c12da15c95c3c51b4fccc4f0aa01912866310b
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-11-06 22:11:44 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import { createMuiTheme, StylesProvider, ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+
+import { store } from './redux/store';
+
+import { MainLayout } from './components/layout/MainLayout/MainLayout';
+import { Homepage } from './components/views/Homepage/Homepage';
+import { Product } from './components/views/Product/Product';
+import { Shop } from './components/views/Shop/Shop';
+import { Cart } from './components/views/Cart/Cart';
+import { NotFound } from './components/views/NotFound/NotFound';
+import { NoPermission } from './components/views/NoPermission/NoPermission';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#2B4C6F' },
+  },
+});
+
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <MainLayout>
+            <Switch>
+              <Route exact path='/' component={Homepage} />
+              <Route exact path='/product/:id' component={Product} />
+              <Route exact path='/shop' component={Shop} />
+              <Route exact path='/cart' component={Cart} />
+              <Route exact path='/no-permission' component={NoPermission} />
+              <Route path='*' component={NotFound} />
+            </Switch>
+          </MainLayout>
+        </ThemeProvider>
+      </StylesProvider>
+    </BrowserRouter>
+  </Provider>
+);
+
+export { App };
